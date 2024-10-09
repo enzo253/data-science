64f2d76f16f41b0b0c6560f5a6ee46d05694f097
enzo253 <139024939+enzo253@users.noreply.github.com>
2024-10-09 10:59:54 +0200

    renaming and organising

--- Sprint_Projects/practise.py
+++
@@ -1,6 +0,0 @@
-
-def master_yoda(text):
-    lst = text.split(), lst.reverse()
-    return " ".join(lst)
-
-master_yoda("i am home")
--- Sprint_Projects/print_03.py
+++ Sprint_Projects/print_03.py
@@ -7,26 +7,25 @@ for num in input_array:
 print("Input Array:", array_00)
 
 
-mean = sum(array_00) / len(array_00)
-
 def find_std_dev():
     if len(array_00) <= 1:
-        return None  
+        return None
 
+    mean = sum(array_00) / len(array_00)
 
     total_of_squares = sum((num - mean) ** 2 for num in array_00)
 
     variance = total_of_squares / len(array_00)
 
+    array_00.sort()
+
     if len(array_00) % 2 == 0 :
-        number = len(array_00) / 2
-        fin_num = array_00[number] + array_00[number+2] / 2
-        print(f'Median{fin_num}')
+        mid_index = len(array_00) // 2
+        median = array_00[mid_index] + array_00[mid_index - 1] / 2
 
     else: 
-        number_01 = len(array_00) / 2
-        fin_num_01 = array_00[number_01] / 2
-        print(f'Median{fin_num_01}')
+        mid_index = len(array_00) // 2  
+        median = array_00[mid_index]
 
 
     
@@ -46,3 +45,17 @@ if results:
     print(f"Standard Deviation: {std_dev}")
 else:
     print("Not enough data to calculate statistics.")
+
+
+#Sprint 02
+
+#Ex _02 = increase
+#Ex_03 = E
+#Ex_04 = 10
+#Ex_05 = skewed left
+#Ex_06 = 15,000
+#Ex_07 = 35,000
+#Ex_08 = D
+#Ex_09 = Standard Deviation: 10.44030650891055
+#Ex_10 = B
+#Ex_11 = D
