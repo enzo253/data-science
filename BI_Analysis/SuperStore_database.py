dd0088b345c4fc896cfc4579655f6ffdb6724e88
enzo253 <139024939+enzo253@users.noreply.github.com>
2025-01-08 20:01:01 +0100

    finished filling in sql databese after finishing the design

--- BI_Analysis/SuperStore_database.py
+++ BI_Analysis/SuperStore_database.py
@@ -6,12 +6,17 @@ csv = pd.read_csv("/Users/enzowurtele/Desktop/DATABASES/Sample-Superstore.csv", 
 
 # Create DataFrames
 customer = csv[['Customer ID', 'Customer Name', 'City', 'State', 'Postal Code', 'Region', 'Segment']]
-orders = csv[['Order ID', 'Order Date', 'Ship Date', 'Ship Mode']]
+
+orders = csv[['Order ID','Customer ID', 'Order Date', 'Ship Date', 'Ship Mode']]
+
 products = csv[['Product ID', 'Category', 'Sub-Category', 'Product Name']]
+
 order_details = csv[['Order ID', 'Product ID', 'Sales', 'Quantity', 'Discount', 'Profit']]
 
+customer_clean = customer.drop_duplicates()
+
 # Connect to SQLite database
-conn = sqlite3.connect('superstore.db')
+conn = sqlite3.connect('SuperStore.db')
 cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS Customer (
@@ -22,25 +27,44 @@ State TEXT,
 PostalCode TEXT,
 Region TEXT,
 Segment TEXT
-)
+);
 ''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS Products (
+cursor.execute('''CREATE TABLE IF NOT EXISTS Orders (
+CustomerID TEXT,
+OrderID TEXT PRIMARY KEY,
+OrderDate TEXT,
+ShipDate TEXT,
+ShipMode TEXT,
+FOREIGN KEY (CustomerID) REFERENCES customer (CustomerID)
+);
+''')
+
+cursor.execute(''' CREATE TABLE IF NOT EXISTS Products (
 ProductID TEXT PRIMARY KEY,
 Category TEXT,
 SubCategory TEXT,
 ProductName TEXT
-)
+);
 ''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS Orders (
-OrderID TEXT PRIMARY KEY,
-OrderDate TEXT,
-ShipDate TEXT,
-ShipMode TEXT
-)
+cursor.execute('''CREATE TABLE IF NOT EXISTS Order_details (
+OrderID TEXT,
+ProductID TEXT,
+Sales REAL,
+Quantity INTEGER,
+Discount REAL,
+Profit REAL,
+FOREIGN KEY (OrderID) REFERENCES orders (OrderID),
+FOREIGN KEY (ProductID) REFERENCES products (ProductID)
+);
 ''')
 
+customer_clean.to_sql('Customer', conn, if_exists='replace', index=False)
 
+orders.to_sql('Orders', conn, if_exists='replace', index=False)
 
+products.to_sql('Products', conn, if_exists='replace', index=False)
+
+order_details.to_sql('Order_details', conn, if_exists='replace', index=False)
 
